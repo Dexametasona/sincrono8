4535fed6f8fb3e3b57d174809c88aad4c6336a50
Dexametasona <machicadogomezalexander@gmail.com>
2022-12-30 09:36:55 -0500

    creacion de directivas

--- src/app/componente/login/login.component.ts
+++ src/app/componente/login/login.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  name='';
+  pass='';
   constructor() { }
+  validar(){
+  }
 
   ngOnInit(): void {
   }
